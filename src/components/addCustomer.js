69d4f23b5a40f756f354650acb51ef519fbf555d
Taras <tsamoilenko97@gmail.com>
2018-10-30 01:12:59 +0200

    added  maxLength  required and date validation

--- src/components/addCustomer.js
+++ src/components/addCustomer.js
@@ -2,34 +2,34 @@ import React, { Component } from 'react';
 import '../css/customer.css';
 
 class AddCustomer extends Component {
-   constructor(){
-      super();
-      this.state ={
-         customerName :'',
-         phoneNumber: '',
-         adderss: ''
-      }
-   }
-   handleInputChange = (e) => {
-      this.setState({ [e.target.name]: e.target.value });
-   }
-   handleCustomerFormSubmit = (e) => {
-      e.preventDefault();
-      console.log(this.state);
-      this.props.updateCustomerList(this.state);
-   }
-   render() {
+  constructor() {
+    super();
+    this.state = {
+      customerName: '',
+      phoneNumber: '',
+      adderss: ''
+    }
+  }
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  handleCustomerFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.props.updateCustomerList(this.state);
+  }
+  render() {
     return (
       <div className="addCustomer">
-         <form onSubmit={this.handleCustomerFormSubmit}>
-            <label>Customer Name :</label>
-            <input type="text" name="customerName" onChange={this.handleInputChange}/>
-            <label>Phone Number :</label>
-            <input type="text" name="phoneNumber" onChange={this.handleInputChange}/>
-            <label>Adderss :</label>
-            <input type="text" name="adderss" onChange={this.handleInputChange}/>
-            <input className="addCustomerBtn" type="submit" name="submit_customer_Data"/>
-         </form>
+        <form onSubmit={this.handleCustomerFormSubmit}>
+          <label>Customer Name :</label>
+          <input type="text" name="customerName" onChange={this.handleInputChange} />
+          <label>Phone Number :</label>
+          <input type="text" name="phoneNumber" onChange={this.handleInputChange} />
+          <label>Adderss :</label>
+          <input type="text" name="adderss" onChange={this.handleInputChange} />
+          <input className="addCustomerBtn" type="submit" name="submit_customer_Data" />
+        </form>
       </div>
     );
   }
--- src/components/addOrder.js
+++ src/components/addOrder.js
@@ -3,89 +3,89 @@ import '../css/addOrder.css';
 
 
 class addOrders extends Component {
-   constructor(){
-      super();
+  constructor() {
+    super();
 
-      this.state = {
-            _id : null,
-            orderName: '',
-            customerName: '',
-            Description: '',
-            dueDate: '',
-            orderDate: '',
-            status: ''
-      }
-   }
-   handleInputChange = (e) => {
-      console.log(e.target.value);
-      this.setState({ [e.target.name]: e.target.value });
-   }
-   handleFormSubmit = (e) => {
-      e.preventDefault();
-      //let date =  new Date();
-      this.setState({
-         orderDate : '20-30-2019',
-         status: 'Pending',
-         _id : Math.floor(Math.random() * 10000000)
-      },() => {
-         console.log(this.state);
-         this.props.handleOrderUpdate(this.state);
-         this.props.handleHideForm();
-      })
-   }
+    this.state = {
+      _id: null,
+      orderName: '',
+      customerName: '',
+      Description: '',
+      dueDate: '',
+      orderDate: '',
+      status: ''
+    }
+  }
+  handleInputChange = (e) => {
+    console.log(e.target.value);
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    //let date =  new Date();
+    this.setState({
+      orderDate: '20-30-2019',
+      status: 'Pending',
+      _id: Math.floor(Math.random() * 10000000)
+    }, () => {
+      console.log(this.state);
+      this.props.handleOrderUpdate(this.state);
+      this.props.handleHideForm();
+    })
+  }
   render() {
     return (
       <div className="addOrderFormContainer">
-         <form onSubmit={this.handleFormSubmit}>
-            < div className = "field orderName" >
-              < input type = "text"
-              placeholder = "Name"
-              name = "orderName"
-              onChange = {
+        <form onSubmit={this.handleFormSubmit}>
+          <div className="field orderName" >
+            <input type="text"
+              placeholder="Name"
+              name="orderName"
+              onChange={
                 this.handleInputChange
               }
-              />
-            </div>
-            <div className="field description">
-              < input type = "text"
-              placeholder = "Description"
-              name = "Description"
-              onChange = {
+            />
+          </div>
+          <div className="field description">
+            <input type="text"
+              placeholder="Description"
+              name="Description"
+              onChange={
                 this.handleInputChange
               }
-              />
-            </div>
-            <div className="field duedate">
-                < input type = "date"
-                placeholder = "DueDate"
-                name = "dueDate"
-                onChange = {
-                  this.handleInputChange
-                }
-                />
-            </div>
-            <div className="field customername">
-                < select name = "customerName"
-                onChange = {
-                    this.handleInputChange
+            />
+          </div>
+          <div className="field duedate">
+            <input type="date"
+              placeholder="DueDate"
+              name="dueDate"
+              onChange={
+                this.handleInputChange
+              }
+            />
+          </div>
+          <div className="field customername">
+            <select name="customerName"
+              onChange={
+                this.handleInputChange
+              } > {
+                this.props.customers && this.props.customers.map((customer, key) => (<
+                  option key={
+                    key
                   } > {
-                    this.props.customers && this.props.customers.map((customer, key) => ( <
-                      option key = {
-                        key
-                      } > {
-                        customer.customerName
-                      } < /option>
-                    ))
+                    customer.customerName
+                  } < /option>
+                ))
                   } <
                   /select>
             </div>
-            <div className="action-container" >
-              <input type="submit" className="action primary" />
-            </div>
+                  <div className="action-container" >
+                    <input type="submit" className="action primary" />
+                  </div>
          </form>
       </div>
-    );
-  }
-}
-
-export default addOrders;
+            );
+          }
+        }
+        
+        export default addOrders;
