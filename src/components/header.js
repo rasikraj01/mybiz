4e522f71d00e9ff259a78bf0837f7aa93f2a9477
Taras <tsamoilenko97@gmail.com>
2018-10-30 16:33:52 +0200

    made navbar responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import '../css/header.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
-      <div className="navbar">
-         <ul>
+      <nav className="navbar">
+
+        <input type="checkbox" id="nav-menu-checkbox" className="nav-menu" />
+        <label htmlFor="nav-menu-checkbox" className="nav-menu-label">
+          <div className="burger-menu">
+            <div className="burger-menu-line"></div>
+            <div className="burger-menu-line"></div>
+            <div className="burger-menu-line"></div>
+          </div>
+        </label>
+
+        <ul className="nav-list">
+          <li className="nav-list-li">
             <Link to="/">Home</Link>
+          </li>
+          <li className="nav-list-li">
             <Link to="/inprogress">In Progress</Link>
+          </li>
+          <li className="nav-list-li">
             <Link to="/pending/">Pending Orders</Link>
+          </li>
+          <li className="nav-list-li">
             <Link to="/delivered/">Delivered Orders</Link>
+          </li>
+          <li className="nav-list-li">
             <Link to="/customers/">Customers</Link>
+          </li>
+          <li className="nav-list-li">
             <Link to="/analytics/">Analytics</Link>
-         </ul>
-      </div>
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
